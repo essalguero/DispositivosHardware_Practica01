00b95aac24bc1cfbf433b9cccb36e383f5636c99
LIVEU-TAD\eugenio.salguero <eugenio.salguero@live.u-tad.com>
2018-01-02 00:28:31 +0100

    Added functionality for the Main Menu

--- HogansAlley/Assets/HogansAlley/Scripts/GameplayManager.cs
+++ HogansAlley/Assets/HogansAlley/Scripts/GameplayManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameplayManager : MonoBehaviour {
 
@@ -22,7 +23,12 @@ public class GameplayManager : MonoBehaviour {
 	void Update () {
 		if (Time.realtimeSinceStartup > (playingTime + timeZero))
         {
-            Application.Quit();
+            //Application.Quit();
+            SceneManager.LoadScene(0);
+
+            MainMenu mainMenu = MainMenu.GetInstance();
+
+            mainMenu.AddNewScore(points);
         }
 
         textMesh.text = "Time: " + (int)(Time.realtimeSinceStartup - timeZero) + "\nScore: " + points;
---
+++ HogansAlley/Assets/HogansAlley/Scripts/MainMenu.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using UnityEngine.UI;
+
+
+public class MainMenu : MonoBehaviour {
+
+    private static MainMenu instance;
+
+    private List<float> scoreList = new List<float>();
+
+    public Text[] scoreTextFields;
+    public Canvas menuCanvas;
+
+    bool updated = false;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            MainMenu.GetInstance().ActivateCanvas();
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+
+        for(int i = 0; i < scoreTextFields.Length; i++)
+        {
+            AddNewScore(float.MinValue);
+        }
+
+        updated = true;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (updated)
+        {
+            if (scoreList[scoreList.Count - 1] == float.MinValue)
+            {
+                scoreTextFields[0].text = "1. -";
+            }
+            else
+            {
+                scoreTextFields[0].text = "1. " + scoreList[scoreList.Count - 1].ToString();
+            }
+
+            if (scoreList[scoreList.Count - 2] == float.MinValue)
+            {
+                scoreTextFields[1].text = "2. -";
+            }
+            else
+            {
+                scoreTextFields[1].text = "2. " + scoreList[scoreList.Count - 2].ToString();
+            }
+
+            if (scoreList[scoreList.Count - 3] == float.MinValue)
+            {
+                scoreTextFields[2].text = "3. -";
+            }
+            else
+            {
+                scoreTextFields[2].text = "3. " + scoreList[scoreList.Count - 3].ToString();
+            }
+
+            updated = false;
+        }
+	}
+
+    public void LoadScene()
+    {
+        menuCanvas.enabled = false;
+        SceneManager.LoadScene(1);
+    }
+
+    public void AddNewScore(float newScore)
+    {
+        scoreList.Add(newScore);
+
+        scoreList.Sort();
+
+        updated = true;
+    }
+
+    public static MainMenu GetInstance()
+    {
+        return instance;
+    }
+
+    public void ActivateCanvas()
+    {
+        menuCanvas.enabled = true;
+    }
+}
